e9c5d74031db8c0cf3fe4b64d135563efafe2b66
AndyER03 <AndyER03@outlook.com>
2021-01-29 22:47:57 +0300

    Added get headers

--- BandyFW/MainActivity.cs
+++ BandyFW/MainActivity.cs
@@ -3,7 +3,6 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Widget;
 using System;
-using System.Net;
 using System.Net.Http;
 
 namespace BandyFW
@@ -21,13 +20,30 @@ namespace BandyFW
             Button button2 = FindViewById<Button>(Resource.Id.button2);
             Button button3 = FindViewById<Button>(Resource.Id.button3);
             EditText edittext = FindViewById<EditText>(Resource.Id.edittext1);
-            TextView textview = FindViewById<TextView>(Resource.Id.textView1);
+            EditText edittext2 = FindViewById<EditText>(Resource.Id.edittext2);
 
             HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage();
             request.RequestUri = new Uri("https://api-mifit-ru.huami.com/devices/ALL/hasNewVersion?productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=257&userid=0&userId=0&deviceSource=25&fontVersion=0&fontFlag=0&appVersion=4.8.1_50392&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=0&timezone=0&v=0");
             request.Method = HttpMethod.Get;
-            //request.Headers.Add("Accept", "application/json");
+
+            request.Headers.Add("hm-privacy-diagnostics", "0");
+            request.Headers.Add("country", "0");
+            request.Headers.Add("appplatform", "android_phone");
+            request.Headers.Add("hm-privacy-ceip", "0");
+            request.Headers.Add("X-Request-Id", "0");
+            request.Headers.Add("timezone", "0");
+            request.Headers.Add("channel", "0");
+            request.Headers.Add("User-Agent", "0");
+            request.Headers.Add("cv", "0");
+            request.Headers.Add("appname", "com.xiaomi.hm.health");
+            request.Headers.Add("v", "0");
+            request.Headers.Add("apptoken", "0");
+            request.Headers.Add("lang", "0");
+            request.Headers.Add("Host", "api-mifit-ru.huami.com");
+            request.Headers.Add("Connection", "0");
+            request.Headers.Add("Accept-Encoding", "0");
+
             HttpResponseMessage response = await client.SendAsync(request);
             HttpContent responseContent = response.Content;
             var json = await responseContent.ReadAsStringAsync();
@@ -56,7 +72,7 @@ namespace BandyFW
 
             button3.Click += delegate
             {
-                textview.Text = json;
+                edittext2.Text = json;
                 Toast.MakeText(this, "Created by AndyER03", ToastLength.Short).Show();
             };
         }
