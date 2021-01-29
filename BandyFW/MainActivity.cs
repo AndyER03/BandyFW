d517ff79194ac3cf0823cc69a36942f846bf94e9
AndyER03 <AndyER03@outlook.com>
2021-01-30 00:21:12 +0300

    Control commit

--- BandyFW/MainActivity.cs
+++ BandyFW/MainActivity.cs
@@ -16,15 +16,28 @@ namespace BandyFW
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
-            Button button = FindViewById<Button>(Resource.Id.button1);
-            Button button2 = FindViewById<Button>(Resource.Id.button2);
-            Button button3 = FindViewById<Button>(Resource.Id.button3);
-            EditText edittext = FindViewById<EditText>(Resource.Id.edittext1);
-            EditText edittext2 = FindViewById<EditText>(Resource.Id.edittext2);
+            Button submit_button = FindViewById<Button>(Resource.Id.submit_button);
+            EditText model_text = FindViewById<EditText>(Resource.Id.model_text);
+            EditText production_text = FindViewById<EditText>(Resource.Id.production_text);
+            EditText app_name_text = FindViewById<EditText>(Resource.Id.app_name_text);
+            EditText app_version_text = FindViewById<EditText>(Resource.Id.app_version_text);
+            EditText response_text = FindViewById<EditText>(Resource.Id.response_text);
 
             HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage();
-            request.RequestUri = new Uri("https://api-mifit-ru.huami.com/devices/ALL/hasNewVersion?productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=257&userid=0&userId=0&deviceSource=25&fontVersion=0&fontFlag=0&appVersion=4.8.1_50392&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=0&timezone=0&v=0");
+
+            UriBuilder uriBuilder = new UriBuilder
+            {
+                Scheme = "https",
+                Host = "api-mifit-ru.huami.com",
+                Path = "devices/ALL/hasNewVersion",
+                Query = "productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=" + production_text.Text + "&userid=0&userId=0&deviceSource=" + model_text.Text + "&fontVersion=0&fontFlag=0&appVersion=" + app_version_text.Text + "&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=0&timezone=0&v=0",
+            };
+            Uri URL = uriBuilder.Uri;
+            String stringUri;
+            stringUri = URL.ToString();
+
+            request.RequestUri = new Uri(stringUri);
             request.Method = HttpMethod.Get;
 
             request.Headers.Add("hm-privacy-diagnostics", "0");
@@ -36,7 +49,7 @@ namespace BandyFW
             request.Headers.Add("channel", "0");
             request.Headers.Add("User-Agent", "0");
             request.Headers.Add("cv", "0");
-            request.Headers.Add("appname", "com.xiaomi.hm.health");
+            request.Headers.Add("appname", "{app_name_text}");
             request.Headers.Add("v", "0");
             request.Headers.Add("apptoken", "0");
             request.Headers.Add("lang", "0");
@@ -53,28 +66,15 @@ namespace BandyFW
             //var url = $"{hostname}productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=257&userid=0&userId=0&deviceSource=25&fontVersion=0&fontFlag=0&appVersion=4.8.1_50392&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=0&timezone=0&v=0";
             //var responseString = web.DownloadString(url);
 
-            button.Click += delegate
+            submit_button.Click += delegate
             {
-				if (edittext.Text == "") {
-                    button.Text = "Click";
-                    Toast.MakeText(this, "Enter value", ToastLength.Short).Show();
+				if (model_text.Text != "" && model_text.Text != "" && model_text.Text != "" && model_text.Text != "") {
+                    response_text.Text = stringUri + json;
                 }
-                else
-				    button.Text = edittext.Text;
-
+                Toast.MakeText(this, "You should to input all required values!", ToastLength.Long).Show();
             };
 
-            button2.Click += delegate
-            {
-                button.Text = "Click";
-                edittext.Text = "";
-            };
-
-            button3.Click += delegate
-            {
-                edittext2.Text = json;
-                Toast.MakeText(this, "Created by AndyER03", ToastLength.Short).Show();
-            };
         }
+
     }
 }
