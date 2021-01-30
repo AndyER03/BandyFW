121ade5aa6e9fd091cbae2f3030a181bc2f481b2
AndyER03 <AndyER03@outlook.com>
2021-01-30 15:40:41 +0300

    Control commit

--- BandyFW/MainActivity.cs
+++ BandyFW/MainActivity.cs
@@ -5,6 +5,7 @@ using Android.Widget;
 using System;
 using System.Net;
 using System.Net.Http;
+using Xamarin.Essentials;
 
 namespace BandyFW
 {
@@ -16,8 +17,9 @@ namespace BandyFW
 			base.OnCreate(savedInstanceState);
 			Xamarin.Essentials.Platform.Init(this, savedInstanceState);
 			SetContentView(Resource.Layout.activity_main);
-		}
-		protected override void OnResume()
+        }
+
+        protected override void OnResume()
         {
             base.OnResume();
             Button submit_button = FindViewById<Button>(Resource.Id.submit_button);
@@ -29,55 +31,63 @@ namespace BandyFW
 
             submit_button.Click += async delegate
             {
-                if (model_text.Text != "" && model_text.Text != "" && model_text.Text != "" && model_text.Text != "")
+                var current = Connectivity.NetworkAccess;
+                if (current == Xamarin.Essentials.NetworkAccess.Internet)
                 {
-                    HttpClient client = new HttpClient();
-                    HttpRequestMessage request = new HttpRequestMessage();
-
-                    UriBuilder uriBuilder = new UriBuilder
+                    if (model_text.Text == "" || model_text.Text == "" || model_text.Text == "" || model_text.Text == "")
                     {
-                        Scheme = "https",
-                        Host = "api-mifit-ru.huami.com",
-                        Path = "devices/ALL/hasNewVersion",
-                        Query = "productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=" + production_text.Text + "&userid=0&userId=0&deviceSource=" + model_text.Text + "&fontVersion=0&fontFlag=0&appVersion=" + app_version_text.Text + "&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=0&timezone=0&v=0",
-                    };
-                    Uri URL = uriBuilder.Uri;
-                    String stringUri;
-                    stringUri = URL.ToString();
-
-                    request.RequestUri = new Uri(stringUri);
-                    request.Method = HttpMethod.Get;
-
-                    request.Headers.Add("hm-privacy-diagnostics", "0");
-                    request.Headers.Add("country", "0");
-                    request.Headers.Add("appplatform", "android_phone");
-                    request.Headers.Add("hm-privacy-ceip", "0");
-                    request.Headers.Add("X-Request-Id", "0");
-                    request.Headers.Add("timezone", "0");
-                    request.Headers.Add("channel", "0");
-                    request.Headers.Add("User-Agent", "0");
-                    request.Headers.Add("cv", "0");
-                    request.Headers.Add("appname", app_name_text.Text);
-                    request.Headers.Add("v", "0");
-                    request.Headers.Add("apptoken", "0");
-                    request.Headers.Add("lang", "0");
-                    request.Headers.Add("Host", "api-mifit-ru.huami.com");
-                    request.Headers.Add("Connection", "0");
-                    request.Headers.Add("Accept-Encoding", "0");
-
-                    HttpResponseMessage response = await client.SendAsync(request);
-                    if (response.StatusCode == HttpStatusCode.OK)
+                        Toast.MakeText(this, "You should to input all required values!", ToastLength.Long).Show();
+                    }
+                    else
                     {
-                        HttpContent responseContent = response.Content;
-                        var json = await responseContent.ReadAsStringAsync();
-                        response_text.Text = json;
+                        HttpClient client = new HttpClient();
+                        HttpRequestMessage request = new HttpRequestMessage();
+
+                        UriBuilder uriBuilder = new UriBuilder
+                        {
+                            Scheme = "https",
+                            Host = "api-mifit-ru.huami.com",
+                            Path = "devices/ALL/hasNewVersion",
+                            Query = "productId=0&vendorSource=0&resourceVersion=0&firmwareFlag=0&vendorId=0&resourceFlag=0&productionSource=" + production_text.Text + "&userid=0&userId=0&deviceSource=" + model_text.Text + "&fontVersion=0&fontFlag=0&appVersion=" + app_version_text.Text + "&appid=0&callid=0&channel=0&country=0&cv=0&device=0&deviceType=ALL&device_type=0&firmwareVersion=0&hardwareVersion=0&lang=0&support8Bytes=true&timezone=0&v=0",
+                        };
+                        Uri URL = uriBuilder.Uri;
+                        String stringUri;
+                        stringUri = URL.ToString();
+
+                        request.RequestUri = new Uri(stringUri);
+                        request.Method = HttpMethod.Get;
+
+                        request.Headers.Add("hm-privacy-diagnostics", "0");
+                        request.Headers.Add("country", "0");
+                        request.Headers.Add("appplatform", "android_phone");
+                        request.Headers.Add("hm-privacy-ceip", "0");
+                        request.Headers.Add("X-Request-Id", "0");
+                        request.Headers.Add("timezone", "0");
+                        request.Headers.Add("channel", "0");
+                        request.Headers.Add("User-Agent", "0");
+                        request.Headers.Add("cv", "0");
+                        request.Headers.Add("appname", app_name_text.Text);
+                        request.Headers.Add("v", "0");
+                        request.Headers.Add("apptoken", "0");
+                        request.Headers.Add("lang", "0");
+                        request.Headers.Add("Host", "api-mifit-ru.huami.com");
+                        request.Headers.Add("Connection", "0");
+                        request.Headers.Add("Accept-Encoding", "0");
+
+                        HttpResponseMessage response = await client.SendAsync(request);
+                        if (response.StatusCode == HttpStatusCode.OK)
+                        {
+                            HttpContent responseContent = response.Content;
+                            var json = await responseContent.ReadAsStringAsync();
+                            response_text.Text = json;
+                        }
                     }
                 }
-                else {
-                Toast.MakeText(this, "You should to input all required values!", ToastLength.Long).Show();
+				else
+				{
+                    Toast.MakeText(this, "Enable network!", ToastLength.Short).Show();
                 }
             };
         }
-
     }
 }
